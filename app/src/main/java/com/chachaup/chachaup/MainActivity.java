049168bc45407a45eac408a6d610216e9223e056
chacha <chachaerickjo@gmail.com>
2022-05-31 15:30:02 +0300

    ft - add second activity; add onClick event to render second activity

--- app/src/main/java/com/chachaup/chachaup/MainActivity.java
+++ app/src/main/java/com/chachaup/chachaup/MainActivity.java
@@ -2,13 +2,31 @@ package com.chachaup.chachaup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity{
+    @BindView(R.id.buttonLogin)
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/chachaup/chachaup/SignUp.java
@@ -0,0 +1,19 @@
+package com.chachaup.chachaup;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        Intent intent = getIntent();
+
+    }
+}
