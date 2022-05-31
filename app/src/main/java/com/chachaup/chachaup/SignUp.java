802e7c9c89a49d3f4d17fa854aa661e036803b17
chacha <chachaerickjo@gmail.com>
2022-06-01 00:01:54 +0300

    ft - add listener for switching to signin of one already has an account

--- app/src/main/java/com/chachaup/chachaup/SignUp.java
+++ app/src/main/java/com/chachaup/chachaup/SignUp.java
@@ -2,18 +2,38 @@ package com.chachaup.chachaup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class SignUp extends AppCompatActivity {
+    @BindView(R.id.textViewLogin)
+    TextView mSignIn;
+    @BindView(R.id.editTextPersonName)
+    EditText mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+        ButterKnife.bind(this);
 
-        Intent intent = getIntent();
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
+                Toast.makeText(SignUp.this,"Taking you to sign in...",Toast.LENGTH_LONG).show();
+                startActivity(intent);
+            }
+        });
 
     }
+
 }
