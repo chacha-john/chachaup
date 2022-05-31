a2174381a3d25652f3176d3209fe1c751493d0d3
chacha <chachaerickjo@gmail.com>
2022-05-31 23:59:31 +0300

    ft - implement custom adapter and show toast on clicking an item

--- app/src/main/java/com/chachaup/chachaup/Dashboard.java
+++ app/src/main/java/com/chachaup/chachaup/Dashboard.java
@@ -2,7 +2,12 @@ package com.chachaup.chachaup;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +22,8 @@ public class Dashboard extends AppCompatActivity {
     private String[] recipes = new String[] {"Chop your spices into a dry bowl, add tallow to the mixture and allow it to cook for five minutes in medium heat, add the peeled bananas and 1 litre of water and simmer for 25 minutes. Your delicious meal is ready to be served.","Boil the water until it is hot enough to drink, add green tea and allow it to boil for a few more minutes.","Boil the water until it is hot enough to drink, add coffee and allow it to boil for a few more minutes."};
     @BindView(R.id.textViewWelcomeMessageDashboard)
     TextView welcomeMessage;
+    @BindView(R.id.listViewRecipes)
+    ListView mRecipesList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,5 +35,14 @@ public class Dashboard extends AppCompatActivity {
         String email = intent.getStringExtra("email");
         welcomeMessage.setText("Hi,"+ email);
 
+        DashboardArrayAdapter adapter = new DashboardArrayAdapter(this, android.R.layout.simple_list_item_1,meals,ingredients,recipes);
+        mRecipesList.setAdapter(adapter);
+
+        mRecipesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Dashboard.this,"This is a recipe list item!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
