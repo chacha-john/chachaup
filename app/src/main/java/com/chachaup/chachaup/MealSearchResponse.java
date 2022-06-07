c581370697dceb8611db066f867d53103f1ea0c1
chacha <chachaerickjo@gmail.com>
2022-06-08 01:04:33 +0300

    ft - add class that represents a meal search

---
+++ app/src/main/java/com/chachaup/chachaup/MealSearchResponse.java
@@ -0,0 +1,40 @@
+
+package com.chachaup.chachaup;
+
+import java.util.List;
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class MealSearchResponse {
+
+    @SerializedName("meals")
+    @Expose
+    private List<Meal> meals = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public MealSearchResponse() {
+    }
+
+    /**
+     * 
+     * @param meals
+     */
+    public MealSearchResponse(List<Meal> meals) {
+        super();
+        this.meals = meals;
+    }
+
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
+}
