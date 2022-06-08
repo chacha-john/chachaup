7f43220d2335395220138f5a13f21214c286277e
chacha <chachaerickjo@gmail.com>
2022-06-08 10:29:00 +0300

    ft - add a meal model class

---
+++ app/src/main/java/com/chachaup/chachaup/models/Meal.java
@@ -0,0 +1,390 @@
+
+package com.chachaup.chachaup.models;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Meal {
+
+    @SerializedName("idMeal")
+    @Expose
+    private String idMeal;
+    @SerializedName("strMeal")
+    @Expose
+    private String strMeal;
+    @SerializedName("strCategory")
+    @Expose
+    private String strCategory;
+    @SerializedName("strArea")
+    @Expose
+    private String strArea;
+    @SerializedName("strInstructions")
+    @Expose
+    private String strInstructions;
+    @SerializedName("strMealThumb")
+    @Expose
+    private String strMealThumb;
+    @SerializedName("strTags")
+    @Expose
+    private String strTags;
+    @SerializedName("strYoutube")
+    @Expose
+    private String strYoutube;
+    @SerializedName("strIngredient1")
+    @Expose
+    private String strIngredient1;
+    @SerializedName("strIngredient2")
+    @Expose
+    private String strIngredient2;
+    @SerializedName("strIngredient3")
+    @Expose
+    private String strIngredient3;
+    @SerializedName("strIngredient4")
+    @Expose
+    private String strIngredient4;
+    @SerializedName("strIngredient5")
+    @Expose
+    private String strIngredient5;
+    @SerializedName("strIngredient6")
+    @Expose
+    private String strIngredient6;
+    @SerializedName("strIngredient7")
+    @Expose
+    private String strIngredient7;
+    @SerializedName("strIngredient8")
+    @Expose
+    private String strIngredient8;
+    @SerializedName("strIngredient9")
+    @Expose
+    private String strIngredient9;
+    @SerializedName("strIngredient10")
+    @Expose
+    private String strIngredient10;
+    @SerializedName("strMeasure1")
+    @Expose
+    private String strMeasure1;
+    @SerializedName("strMeasure2")
+    @Expose
+    private String strMeasure2;
+    @SerializedName("strMeasure3")
+    @Expose
+    private String strMeasure3;
+    @SerializedName("strMeasure4")
+    @Expose
+    private String strMeasure4;
+    @SerializedName("strMeasure5")
+    @Expose
+    private String strMeasure5;
+    @SerializedName("strMeasure6")
+    @Expose
+    private String strMeasure6;
+    @SerializedName("strMeasure7")
+    @Expose
+    private String strMeasure7;
+    @SerializedName("strMeasure8")
+    @Expose
+    private String strMeasure8;
+    @SerializedName("strMeasure9")
+    @Expose
+    private String strMeasure9;
+    @SerializedName("strMeasure10")
+    @Expose
+    private String strMeasure10;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Meal() {
+    }
+
+    /**
+     * 
+     * @param strMealThumb
+     * @param strIngredient10
+     * @param strYoutube
+     * @param strCategory
+     * @param strArea
+     * @param strMeal
+     * @param strMeasure10
+     * @param strTags
+     * @param idMeal
+     * @param strInstructions
+     * @param strIngredient1
+     * @param strIngredient3
+     * @param strIngredient2
+     * @param strIngredient5
+     * @param strMeasure9
+     * @param strIngredient4
+     * @param strIngredient7
+     * @param strMeasure7
+     * @param strIngredient6
+     * @param strMeasure8
+     * @param strIngredient9
+     * @param strMeasure5
+     * @param strIngredient8
+     * @param strMeasure6
+     * @param strMeasure3
+     * @param strMeasure4
+     * @param strMeasure1
+     * @param strMeasure2
+     */
+    public Meal(String idMeal, String strMeal, String strCategory, String strArea, String strInstructions, String strMealThumb, String strTags, String strYoutube, String strIngredient1, String strIngredient2, String strIngredient3, String strIngredient4, String strIngredient5, String strIngredient6, String strIngredient7, String strIngredient8, String strIngredient9, String strIngredient10, String strMeasure1, String strMeasure2, String strMeasure3, String strMeasure4, String strMeasure5, String strMeasure6, String strMeasure7, String strMeasure8, String strMeasure9, String strMeasure10) {
+        super();
+        this.idMeal = idMeal;
+        this.strMeal = strMeal;
+        this.strCategory = strCategory;
+        this.strArea = strArea;
+        this.strInstructions = strInstructions;
+        this.strMealThumb = strMealThumb;
+        this.strTags = strTags;
+        this.strYoutube = strYoutube;
+        this.strIngredient1 = strIngredient1;
+        this.strIngredient2 = strIngredient2;
+        this.strIngredient3 = strIngredient3;
+        this.strIngredient4 = strIngredient4;
+        this.strIngredient5 = strIngredient5;
+        this.strIngredient6 = strIngredient6;
+        this.strIngredient7 = strIngredient7;
+        this.strIngredient8 = strIngredient8;
+        this.strIngredient9 = strIngredient9;
+        this.strIngredient10 = strIngredient10;
+        this.strMeasure1 = strMeasure1;
+        this.strMeasure2 = strMeasure2;
+        this.strMeasure3 = strMeasure3;
+        this.strMeasure4 = strMeasure4;
+        this.strMeasure5 = strMeasure5;
+        this.strMeasure6 = strMeasure6;
+        this.strMeasure7 = strMeasure7;
+        this.strMeasure8 = strMeasure8;
+        this.strMeasure9 = strMeasure9;
+        this.strMeasure10 = strMeasure10;
+    }
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public String getStrCategory() {
+        return strCategory;
+    }
+
+    public void setStrCategory(String strCategory) {
+        this.strCategory = strCategory;
+    }
+
+    public String getStrArea() {
+        return strArea;
+    }
+
+    public void setStrArea(String strArea) {
+        this.strArea = strArea;
+    }
+
+    public String getStrInstructions() {
+        return strInstructions;
+    }
+
+    public void setStrInstructions(String strInstructions) {
+        this.strInstructions = strInstructions;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+
+    public String getStrTags() {
+        return strTags;
+    }
+
+    public void setStrTags(String strTags) {
+        this.strTags = strTags;
+    }
+
+    public String getStrYoutube() {
+        return strYoutube;
+    }
+
+    public void setStrYoutube(String strYoutube) {
+        this.strYoutube = strYoutube;
+    }
+
+    public String getStrIngredient1() {
+        return strIngredient1;
+    }
+
+    public void setStrIngredient1(String strIngredient1) {
+        this.strIngredient1 = strIngredient1;
+    }
+
+    public String getStrIngredient2() {
+        return strIngredient2;
+    }
+
+    public void setStrIngredient2(String strIngredient2) {
+        this.strIngredient2 = strIngredient2;
+    }
+
+    public String getStrIngredient3() {
+        return strIngredient3;
+    }
+
+    public void setStrIngredient3(String strIngredient3) {
+        this.strIngredient3 = strIngredient3;
+    }
+
+    public String getStrIngredient4() {
+        return strIngredient4;
+    }
+
+    public void setStrIngredient4(String strIngredient4) {
+        this.strIngredient4 = strIngredient4;
+    }
+
+    public String getStrIngredient5() {
+        return strIngredient5;
+    }
+
+    public void setStrIngredient5(String strIngredient5) {
+        this.strIngredient5 = strIngredient5;
+    }
+
+    public String getStrIngredient6() {
+        return strIngredient6;
+    }
+
+    public void setStrIngredient6(String strIngredient6) {
+        this.strIngredient6 = strIngredient6;
+    }
+
+    public String getStrIngredient7() {
+        return strIngredient7;
+    }
+
+    public void setStrIngredient7(String strIngredient7) {
+        this.strIngredient7 = strIngredient7;
+    }
+
+    public String getStrIngredient8() {
+        return strIngredient8;
+    }
+
+    public void setStrIngredient8(String strIngredient8) {
+        this.strIngredient8 = strIngredient8;
+    }
+
+    public String getStrIngredient9() {
+        return strIngredient9;
+    }
+
+    public void setStrIngredient9(String strIngredient9) {
+        this.strIngredient9 = strIngredient9;
+    }
+
+    public String getStrIngredient10() {
+        return strIngredient10;
+    }
+
+    public void setStrIngredient10(String strIngredient10) {
+        this.strIngredient10 = strIngredient10;
+    }
+
+    public String getStrMeasure1() {
+        return strMeasure1;
+    }
+
+    public void setStrMeasure1(String strMeasure1) {
+        this.strMeasure1 = strMeasure1;
+    }
+
+    public String getStrMeasure2() {
+        return strMeasure2;
+    }
+
+    public void setStrMeasure2(String strMeasure2) {
+        this.strMeasure2 = strMeasure2;
+    }
+
+    public String getStrMeasure3() {
+        return strMeasure3;
+    }
+
+    public void setStrMeasure3(String strMeasure3) {
+        this.strMeasure3 = strMeasure3;
+    }
+
+    public String getStrMeasure4() {
+        return strMeasure4;
+    }
+
+    public void setStrMeasure4(String strMeasure4) {
+        this.strMeasure4 = strMeasure4;
+    }
+
+    public String getStrMeasure5() {
+        return strMeasure5;
+    }
+
+    public void setStrMeasure5(String strMeasure5) {
+        this.strMeasure5 = strMeasure5;
+    }
+
+    public String getStrMeasure6() {
+        return strMeasure6;
+    }
+
+    public void setStrMeasure6(String strMeasure6) {
+        this.strMeasure6 = strMeasure6;
+    }
+
+    public String getStrMeasure7() {
+        return strMeasure7;
+    }
+
+    public void setStrMeasure7(String strMeasure7) {
+        this.strMeasure7 = strMeasure7;
+    }
+
+    public String getStrMeasure8() {
+        return strMeasure8;
+    }
+
+    public void setStrMeasure8(String strMeasure8) {
+        this.strMeasure8 = strMeasure8;
+    }
+
+    public String getStrMeasure9() {
+        return strMeasure9;
+    }
+
+    public void setStrMeasure9(String strMeasure9) {
+        this.strMeasure9 = strMeasure9;
+    }
+
+    public String getStrMeasure10() {
+        return strMeasure10;
+    }
+
+    public void setStrMeasure10(String strMeasure10) {
+        this.strMeasure10 = strMeasure10;
+    }
+
+}
