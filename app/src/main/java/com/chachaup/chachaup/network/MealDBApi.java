bb7bc9c35936682a5287f2c0bd3ec29eef4d70a5
chacha <chachaerickjo@gmail.com>
2022-06-09 09:31:30 +0300

    ft - remove the oKHTTP client as no apikey is required

--- app/src/main/java/com/chachaup/chachaup/network/MealDBApi.java
+++ app/src/main/java/com/chachaup/chachaup/network/MealDBApi.java
@@ -1,14 +1,14 @@
-package com.chachaup.chachaup;
+package com.chachaup.chachaup.network;
+
+import com.chachaup.chachaup.models.MealSearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface MealDBApi {
-    @GET("/search.php")
+    @GET("search.php")
     Call<MealSearchResponse> getMeals(
-            @Query("s") String mealName
-
-    );
+            @Query("s") String mealName);
 }
-//ww.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+//www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
--- app/src/main/java/com/chachaup/chachaup/network/MealDBClient.java
+++ app/src/main/java/com/chachaup/chachaup/network/MealDBClient.java
@@ -1,15 +1,10 @@
-package com.chachaup.chachaup;
+package com.chachaup.chachaup.network;
 
 import static com.chachaup.chachaup.Constants.MEAL_DB_BASE_URL;
 
-import com.chachaup.chachaup.models.Meal;
+import com.chachaup.chachaup.Constants;
+import com.chachaup.chachaup.network.MealDBApi;
 
-import java.io.IOException;
-
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,19 +12,8 @@ public class MealDBClient {
     private static Retrofit retrofit = null;
     public static MealDBApi getClient(){
         if (retrofit == null){
-//            OkHttpClient okHttpClient = new OkHttpClient.Builder()
-//                    .addInterceptor(new Interceptor() {
-//                        @Override
-//                        public Response intercept(Chain chain) throws IOException {
-//                            Request request = chain.request().newBuilder()
-//                                    .build();
-//                            return chain.proceed(request);
-//                        }
-//                    })
-//                    .build();
             retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.MEAL_DB_BASE_URL)
-//                    .client(okHttpClient)
+                    .baseUrl(MEAL_DB_BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
