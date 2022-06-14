c21baf546296f4015c8aa5ab3ab4fd3f80875c6d
chacha <chachaerickjo@gmail.com>
2022-06-14 08:18:04 +0300

    ft - have action bar

--- app/src/main/java/com/chachaup/chachaup/Constants.java
+++ app/src/main/java/com/chachaup/chachaup/Constants.java
@@ -2,4 +2,5 @@ package com.chachaup.chachaup;
 
 public class Constants {
     public static final String MEAL_DB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
+    public static final String SEARCHED_MEAL = "searchedMeal";
 }
