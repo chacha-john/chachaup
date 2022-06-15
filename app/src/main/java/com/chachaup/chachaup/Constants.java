fb11aa4393386856f3738147aa1e2c888165dea9
chacha <chachaerickjo@gmail.com>
2022-06-15 14:19:08 +0300

    ft - add constants which act as variable names for the stored information in the DB

--- app/src/main/java/com/chachaup/chachaup/Constants.java
+++ app/src/main/java/com/chachaup/chachaup/Constants.java
@@ -2,5 +2,6 @@ package com.chachaup.chachaup;
 
 public class Constants {
     public static final String MEAL_DB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
-    public static final String SEARCHED_MEAL = "searchedMeal";
+    public static final String FIREBASE_CHILD_SEARCHED_MEAL = "searchedMeal";
+    public static final String FIREBASE_CHILD_MEALS = "meals";
 }
