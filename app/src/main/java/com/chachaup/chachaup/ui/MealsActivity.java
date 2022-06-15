b9076cd6c0778d071a4a089383241f8dd616fdbe
chacha <chachaerickjo@gmail.com>
2022-06-15 15:21:50 +0300

    ft - add search widget logic

--- app/src/main/java/com/chachaup/chachaup/ui/MealsActivity.java
+++ app/src/main/java/com/chachaup/chachaup/ui/MealsActivity.java
@@ -1,18 +1,24 @@
 package com.chachaup.chachaup.ui;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.chachaup.chachaup.Constants;
 import com.chachaup.chachaup.adapters.MealsListAdapter;
 import com.chachaup.chachaup.network.MealDBApi;
 import com.chachaup.chachaup.network.MealDBClient;
@@ -40,43 +46,57 @@ public class MealsActivity extends AppCompatActivity {
 
     public List<Meal> mealsList;
 
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentRecipe;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meals);
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
-        String meal = intent.getStringExtra("meal");
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentRecipe = mSharedPreferences.getString(Constants.PREFERENCES_MEAL_KEY, null);
 
-        MealDBApi client = MealDBClient.getClient();
-        Call<MealSearchResponse> call = client.getMeals(meal);
-        call.enqueue(new Callback<MealSearchResponse>() {
+        if (mRecentRecipe != null){
+            fetchMealRecipes(mRecentRecipe);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search,menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.actionSearch);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onResponse(Call<MealSearchResponse> call, Response<MealSearchResponse> response) {
-                hideProgressBar();
-                if(response.isSuccessful()){
-                    mealsList = response.body().getMeals();
-                    mAdapter = new MealsListAdapter(MealsActivity.this, mealsList);
-                    mRecyclerView.setAdapter(mAdapter);
-                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MealsActivity.this);
-                    mRecyclerView.setLayoutManager(layoutManager);
-                    mRecyclerView.setHasFixedSize(true);
-
-                    showRecipe();
-                } else {
-                    showUnsuccessfulMessage();
-                }
+            public boolean onQueryTextSubmit(String query) {
+                addToSharedPreferences(query);
+                fetchMealRecipes(query);
+                return false;
             }
 
             @Override
-            public void onFailure(Call<MealSearchResponse> call, Throwable t) {
-                Log.e(TAG,"Error",t);
-                hideProgressBar();
-                showFailureMessage();
-
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     private void showFailureMessage() {
@@ -95,6 +115,40 @@ public class MealsActivity extends AppCompatActivity {
 
     private void hideProgressBar() {
         mProgressBar.setVisibility(View.GONE);
+    }
+
+    private void addToSharedPreferences(String meal){
+        mEditor.putString(Constants.PREFERENCES_MEAL_KEY,meal).apply();
+    }
+
+    private void fetchMealRecipes(String query){
+        MealDBApi client = MealDBClient.getClient();
+        Call<MealSearchResponse> call = client.getMeals(query);
+        call.enqueue(new Callback<MealSearchResponse>() {
+            @Override
+            public void onResponse(Call<MealSearchResponse> call, Response<MealSearchResponse> response) {
+                hideProgressBar();
+
+                if (response.isSuccessful()){
+                    mealsList = response.body().getMeals();
+                    mAdapter = new MealsListAdapter(MealsActivity.this, mealsList);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MealsActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showRecipe();
+
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MealSearchResponse> call, Throwable t) {
+
+            }
+        });
     }
 
 
