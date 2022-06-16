5e9a59011782e69ed5d3fa483d91fc3f6ca1c504
chacha <chachaerickjo@gmail.com>
2022-06-16 13:22:37 +0300

    ft - use tab layout for login and sign up

---
+++ app/src/main/java/com/chachaup/chachaup/ui/CreateAccountTabFragment.java
@@ -0,0 +1,66 @@
+package com.chachaup.chachaup.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chachaup.chachaup.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CreateAccountTabFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CreateAccountTabFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public CreateAccountTabFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CreateAccountTabFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CreateAccountTabFragment newInstance(String param1, String param2) {
+        CreateAccountTabFragment fragment = new CreateAccountTabFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_account_tab, container, false);
+    }
+}
