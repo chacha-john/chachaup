19cb1c57feffb428403425fc0f0752fa2feaca4d
chacha <chachaerickjo@gmail.com>
2022-06-23 23:20:06 +0300

    ft - add query index

--- app/src/main/java/com/chachaup/chachaup/Constants.java
+++ app/src/main/java/com/chachaup/chachaup/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String MEAL_DB_BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
     public static final String FIREBASE_CHILD_MEALS = "meals";
     public static final String PREFERENCES_MEAL_KEY = "preferencesRecipe";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 }
